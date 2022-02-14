1d719ca490211d0acb786e0a3c412e22ce525358
Javier G <javier.guajardoj@gmail.com>
2022-02-14 02:16:50 +0100

    Replace text

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -7,7 +7,7 @@ export default function Search() {
         <i className="fa fa-search" />
       </div>
       <div className="h-full w-full">
-        <input className="bg-gray-200 w-full h-full rounded-r-lg p-2 text-sm" type="text" placeholder="Find contact" />
+        <input className="bg-gray-200 w-full h-full rounded-r-lg p-2 text-sm" type="text" placeholder="Search" />
       </div>
     </div>
   );
