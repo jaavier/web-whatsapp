1c3bf29d23f3ab437b55cae47f2a5328db0db3fd
Javier GJ <javier.guajardoj@gmail.com>
2022-09-27 11:06:54 +0000

    Hide components when visitor is in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import Sidebar from './components/Sidebar';
 export default function App() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="border-b flex items-center bg-gray-200">
-        <Navbar />
+      <div className="md:hidden sm:block">
+        <div className="text-center">Responsive not available</div>
       </div>
-      <div className="flex h-screen">
-        <div className="w-1/3 border-r">
-          <Sidebar />
+      <div className="md:block sm:hidden">
+        <div className="border-b flex items-center bg-gray-200">
+          <Navbar />
         </div>
-        <div className="w-full relative flex">
-          <Chat />
+        <div className="flex h-screen">
+          <div className="w-1/3 border-r">
+            <Sidebar />
+          </div>
+          <div className="w-full relative flex">
+            <Chat />
+          </div>
         </div>
       </div>
     </div>
